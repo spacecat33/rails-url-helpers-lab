e7eeff67510549968cfbceb26b5ae853e9001306
Janan Akkad <jananakkad@hotmail.co.uk>
2022-11-02 16:28:31 +0000

    commit tree d83688c002a48df18bf9f822ff1f67189c8b0d3f

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # resources :students, only: [:index, :show]
+ # This show route can be refactored into the above resources method call, like so:
+  # resources :students, only: [:index, :show]
+  # However, for the sake of this lab and seeing how you can pass params through
+  # the route, we'll keep it explicit for now and refactor later.
+  
+  
+  resources :students, only: :index
+  get "students/:id", to: "students#show", as: 'student'
+  get '/students/:id/activate', to: 'students#activate', as: 'activate_student'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Student.create(first_name: 'Jane', last_name: 'Smith') 
+Student.create(first_name: 'Tim', last_name: 'Smith') 
+Student.create(first_name: 'John', last_name: 'Smith') 
+Student.create(first_name: 'Janet', last_name: 'Smith') 
